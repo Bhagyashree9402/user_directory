ea6d446b4f5f2e85c7a8c8a3fd64c90e39b61b73
unknown <bhagyashree9402@gmail.com>
2020-08-24 17:30:11 -0700

    Updates

---
+++ precache-manifest.fbeb5c38ea05149251f5f2ac11549f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b480d27e4585dcf8093e60fb1f3210a9",
+    "url": "/user_directory/index.html"
+  },
+  {
+    "revision": "8159b150ac9e92bb6a7e",
+    "url": "/user_directory/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "52770042992ee5fd81e6",
+    "url": "/user_directory/static/js/2.e4124e8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user_directory/static/js/2.e4124e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8159b150ac9e92bb6a7e",
+    "url": "/user_directory/static/js/main.51d785bb.chunk.js"
+  },
+  {
+    "revision": "be019de1aafce4f906ff",
+    "url": "/user_directory/static/js/runtime-main.b9b06cee.js"
+  }
+]);
